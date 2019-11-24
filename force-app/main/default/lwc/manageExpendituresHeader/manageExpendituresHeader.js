0f3b647f52a5f24efbc0941df371f30d9896c098
thom <thom.behrens94@gmail.com>
2019-11-24 15:01:06 -0500

    giving up on navigation for now

--- force-app/main/default/lwc/manageExpendituresHeader/manageExpendituresHeader.js
+++ force-app/main/default/lwc/manageExpendituresHeader/manageExpendituresHeader.js
@@ -1,39 +1,6 @@
-import { LightningElement, api, track } from "lwc";
-import { NavigationMixin } from "lightning/navigation";
+import { LightningElement, api } from "lwc";
 
-export default class ManageExpendituresHeader extends NavigationMixin(
-  LightningElement
-) {
+export default class ManageExpendituresHeader extends LightningElement {
   @api parentname;
-  @api parentid;
-  @track url;
-  @track accountHomePageRef;
 
-  connectedCallback() {
-    // Store the PageReference in a variable to use in handleClick.
-    // This is a plain Javascript object that conforms to the
-    // PageReference type by including "type" and "attributes" properties.
-    // The "state" property is optional.
-    this.accountHomePageRef = {
-      type: "standard__objectPage",
-      attributes: {
-        objectApiName: "Account",
-        actionName: "home"
-      }
-    };
-    this[NavigationMixin.GenerateUrl](this.accountHomePageRef).then(
-      url => (this.url = url)
-    );
-  }
-
-  handleCancel(evt) {
-    // Stop the event's default behavior.
-    // Stop the event from bubbling up in the DOM.
-    console.log(JSON.stringify(this.accountHomePageRef));
-    evt.preventDefault();
-    evt.stopPropagation();
-    // Navigate to the Account Home page.
-    this[NavigationMixin.Navigate](this.accountHomePageRef);
-    console.log('end of handler');
-  }
 }
