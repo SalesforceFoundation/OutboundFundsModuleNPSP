96a2df1424f18287454eaacbbd8c944896e85234
thom <thom.behrens94@gmail.com>
2020-01-18 23:28:26 -0500

    error handling and edge case handling

--- force-app/main/default/lwc/manageExpenditures/manageExpenditures.js
+++ force-app/main/default/lwc/manageExpenditures/manageExpenditures.js
@@ -1,9 +1,11 @@
 import { LightningElement, track, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import getDisbursement from "@salesforce/apex/GauExpendituresManager.getDisbursement";
 import upsertGauExpenditures from "@salesforce/apex/GauExpendituresManager.upsertGauExpenditures";
 export default class ManageExpenditures extends LightningElement(
   LightningElement
 ) {
+  @api objectApiName;
   @api recordId;
   @track parentId;
   @track parentName;
@@ -14,25 +16,42 @@ export default class ManageExpenditures extends LightningElement(
   @track remainingAmountStatus;
   @track disableSave;
   @track error;
+  @track isDisbursement = false;
+  @track loaded = false;
+  @track nonZeroAmount = false;
 
   connectedCallback() {
+    this.isDisbursement = this.objectApiName === "outfunds__Disbursement__c";
+    this.refreshList();
+  }
+
+  refreshList() {
+    this.loaded = false;
+    this.gauExpenditures = [];
     getDisbursement({ disbursementId: this.recordId })
       .then(result => {
         this.parentId = result.recordId;
         this.parentName = result.name;
         this.parentAmount = result.amount;
+        this.nonZeroAmount = result.amount > 0;
         this.gauExpenditures = result.expenditures;
+        this.padEmpty();
         this.updateRemainingAmount();
         this.error = undefined;
+        this.loaded = true;
       })
       .catch(error => {
         this.disbursement = undefined;
         this.error = error;
+        this.loaded = true;
       });
   }
 
   addRow() {
-    let rowId = this.gauExpenditures[this.gauExpenditures.length - 1].rowId + 1;
+    let rowId =
+      this.gauExpenditures.length > 0
+        ? this.gauExpenditures[this.gauExpenditures.length - 1].rowId + 1
+        : 1;
     this.gauExpenditures.push({ rowId: rowId });
   }
 
@@ -47,16 +66,19 @@ export default class ManageExpenditures extends LightningElement(
         this.gauExpenditures[index] = event.detail;
         break;
       case "delete":
-        if (this.gauExpenditures.length > 1) {
-          this.gauExpenditures.splice(index, 1);
-        }
+        this.gauExpenditures.splice(index, 1);
+        this.padEmpty();
         break;
       default:
         break;
     }
-    console.log("what");
     this.updateRemainingAmount();
-    console.log("do");
+  }
+
+  padEmpty() {
+    if (this.gauExpenditures.length === 0) {
+      this.addRow();
+    }
   }
 
   updateRemainingAmount() {
@@ -72,27 +94,74 @@ export default class ManageExpenditures extends LightningElement(
     0);
 
     this.remainingAmount = this.parentAmount - usedAmount;
-    if (this.remainingAmount < 0) {
-      this.remainingAmountStatus = "INVALID";
-      this.disableSave = true;
-    } else {
+    if (this.remainingAmount >= 0) {
       this.remainingAmountStatus = "VALID";
       this.disableSave = false;
+    } else {
+      this.remainingAmountStatus = "INVALID";
+      this.disableSave = true;
     }
   }
 
   handleSave() {
+    if (!this.validate()) {
+      this.showErrorToast("Please remove any incomplete rows, then try again.");
+      return;
+    }
+    if (
+      this.gauExpenditures.length === 1 &&
+      !this.gauExpenditures.gauId &&
+      !this.gauExpenditures.amount
+    ) {
+      this.gauExpenditures = [];
+    }
+    console.log(JSON.stringify(this.gauExpenditures));
+    this.loaded = false;
     upsertGauExpenditures({
       expendituresString: JSON.stringify(this.gauExpenditures),
       disbursementId: this.parentId
     })
       .then(result => {
-        console.log("success");
+        this.showSuccessToast("Expenditures updates successfully!");
+        this.refreshList();
       })
       .catch(error => {
-        console.log("error");
+        this.loaded = true;
         this.disbursement = undefined;
-        this.error = error;
+        this.showErrorToast(error.body.message);
       });
+  }
+
+  validate() {
+    let valid = true;
+    if (this.gauExpenditures.length > 1) {
+      for (const expenditure of this.gauExpenditures) {
+        if (!expenditure.amount || !expenditure.gauId) {
+          valid = false;
+          break;
+        }
+      }
+    }
+
+    return valid;
+  }
+
+  showSuccessToast(message) {
+    const event = new ShowToastEvent({
+      title: "Success!",
+      message: message,
+      variant: "success"
+    });
+    this.dispatchEvent(event);
+  }
+
+  showErrorToast(message) {
+    const event = new ShowToastEvent({
+      title: "Oops! Something Went Wrong",
+      message: message,
+      variant: "error",
+      mode: "dismissable"
+    });
+    this.dispatchEvent(event);
   }
 }
---
+++ force-app/main/default/lwc/warningBanner/warningBanner.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from "lwc";
+
+export default class WarningBanner extends LightningElement {
+  @api message;
+}
