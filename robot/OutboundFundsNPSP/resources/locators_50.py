c2ad533a0387b6eb3975fc90693f570055d2f5d6
gaganpsandhu <gsandhu@salesforce.com>
2021-04-30 08:23:54 -0700

    Adding Summer 21 locators file

--- robot/OutboundFundsNPSP/resources/locators_50.py
+++
@@ -1,6 +0,0 @@
-""" Locators for Winter '21 """
-
-from locators_51 import outboundfundsnpsp_lex_locators
-import copy
-
-outboundfundsnpsp_lex_locators = copy.deepcopy(outboundfundsnpsp_lex_locators)
